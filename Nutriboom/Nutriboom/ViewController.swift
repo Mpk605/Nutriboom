8f5c6c1b32f048a03c312db1a0fc5df083f2a659
Jules Combelles <jcombel@MACD005-05.local>
2022-05-24 15:17:57 +0200

    äpi

--- Nutriboom/Nutriboom/ViewController.swift
+++ Nutriboom/Nutriboom/ViewController.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
-
+    struct NutriScore: Decodable {
+        let code: String
+        let product: String
+        let status: String
+        let status_verbose: String
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let url = URL(string: "https://world.openfoodfacts.org/api/v0/product/737628064502.json")!
+        
+        let task = URLSession.shared.dataTask(with: url) { (data: Data?, response: URLResponse?, error: Error?) -> Void in
+            let json = try? JSONSerialization.jsonObject(with: data!, options: [])
+            
+            print(json["code"])
+        }
+        task.resume()
     }
-
-
 }
 
