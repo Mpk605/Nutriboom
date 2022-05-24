c7ee03b56d1703e7717af89d5826449fd7f43b58
Yannis Amzal <yamzal1@MACD005-06.local>
2022-05-24 13:12:46 +0200

    Yannis dechire

---
+++ Nutriboom/Nutriboom/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nutriboom
+//
+//  Created by Yannis Amzal on 24/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
