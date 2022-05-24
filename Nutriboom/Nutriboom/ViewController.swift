f87f5f42e0923ec8db9ba9cb9f92ed8f9df32722
Louis Collin <lcolli1@MACD005-04.local>
2022-05-24 15:22:50 +0200

    update

--- Nutriboom/Nutriboom/ViewController.swift
+++ Nutriboom/Nutriboom/ViewController.swift
@@ -24,8 +24,10 @@ class ViewController: UIViewController {
         }
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return pictures.count
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
     }
     
     @IBOutlet weak var tableView: UITableView!
