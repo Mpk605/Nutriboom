72237c6971de37c6d464db60fa193695b50e9bac
Marine Kazemi <mkazemi@macd005-07.local>
2022-05-31 11:55:14 +0200

    debut bidouillage

--- Nutriboom/Nutriboom/ViewController.swift
+++ Nutriboom/Nutriboom/ViewController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var table: UITableView!
+
+    let data = ["First","Seconde"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        table.register(MyTableViewCell.nib(), forCellReuseIdentifier: MyTableViewCell.identifier)
+        table.dataSource = self 
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: MyTableViewCell,identifier, for: indexPath) as! MyTableViewCell
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
+    
 }
 
